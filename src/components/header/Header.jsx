b2ce85ccef7755acd545c6a603313cba1818af90
jiuraque <jiulianna.raquel@gmail.com>
2025-04-12 17:33:55 -0400

    construccion del header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,20 @@
-import React from "react";
-import headerImg from "../../assets/imgs/Header.jpg"
+import './Header.css';
+import headerImg from '../../assets/imgs/Header.jpg';
 
-function Header() {
+const Header = () => {
   return (
-    <>
-      <header
-        style={{
-          height: "300px",
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(${headerImg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <h1>¡Pizzería Mamma Mia!</h1>
-        <h3>¡Tenemos las mejores pizzas que podrás encontrar!</h3>
-        <br />
-      </header>
-    </>
+    <header
+      className="header-container d-flex flex-column justify-content-center align-items-center text-center"
+      style={{ backgroundImage: `url(${headerImg})` }}
+    >
+      <div className="w-100 h-100 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+        <h1 className="text-white">¡Pizzería Mamma Mia!</h1>
+        <p className="text-white">¡Las mejores pizzas que podrás encontrar!</p>
+        <hr className="w-50 border-white" />
+      </div>
+    </header>
   );
-}
+};
 
 export default Header;
+
