3670be4ca5f8cbdb17ae418e49c7257be43db120
jiuraque <jiulianna.raquel@gmail.com>
2025-04-11 20:51:38 -0400

    se cambian el nombre de los componentes y se incluyen las tarjetas al home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+import Home from "./components/home/home";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Header />
+      <Home />
     </>
   );
 };
---
+++ src/components/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PizzaCard from '../PizzaCard/PizzaCard'
+
+function Home() {
+  return (
+    <>
+    <PizzaCard/>
+    </>
+  )
+}
+
+export default Home
