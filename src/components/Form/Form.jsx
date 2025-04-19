392d9a784fc3893eb05015a6f5564a7f693a3feb
jiuraque <jiulianna.raquel@gmail.com>
2025-04-19 00:39:32 -0400

    Se crea el archivo form y se agregan el estado de los componentes

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+
+ const Form =() => {
+    const [email, setEmail] = useState('');
+    const [constraseña, setContraseña] = useState('');
+    const [confirmarContraseña, setConfirmarContraseña] = useState('');
+
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Form;
