4a7a71ec4dd4101273d278e69bca6b1df188d67f
jiuraque <jiulianna.raquel@gmail.com>
2025-04-19 01:33:17 -0400

    Se aplican los estados y las validaciones del formulario

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,63 @@
-import {useState} from 'react'
+import { useState } from "react";
 
- const Form =() => {
-    const [email, setEmail] = useState('');
-    const [constraseña, setContraseña] = useState('');
-    const [confirmarContraseña, setConfirmarContraseña] = useState('');
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [confirmarContraseña, setConfirmarContraseña] = useState("");
 
+  const [error, setError] = useState(false);
 
+  const validacion = (e) => {
+    e.preventDefault();
+  };
+
+  if (!email.trim() || !contraseña.trim() || !confirmarContraseña.trim()) {
+    setError(true);
+    return;
+  }
+  setError(false);
+  setEmail("");
+  setContraseña("");
+  setConfirmarContraseña("");
 
   return (
     <>
-    
+      <form className="formulario" onSubmit={validarDatos}>
+        {error ? <p>Todos los campos son obligatorios</p> : null}
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <div className="form-group">
+          <label>Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={(e) => setContraseña(e.target.value)}
+            value={contraseña}
+          />
+        </div>
+        <div className="form-group">
+          <label>Confirmar Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={(e) => setConfirmarContraseña(e.target.value)}
+            value={confirmarContraseña}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Enviar
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default Form;
