9fc3b59bf43a2b23768057e80a13be6b9e7e7df8
jiuraque <jiulianna.raquel@gmail.com>
2025-04-19 20:18:08 -0400

    Se agregan estilos al archivo de Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import Home from "./components/home/home";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 import Registro from "./components/Registro/Registro";
-
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <Header /> */}
-     {/*  <Home /> */}
-     <Registro />
+      {/*  <Home /> */}
+      {/* <Registro /> */}
+      <Login />
       <Footer />
     </>
   );
-};
+}
 
 export default App;
