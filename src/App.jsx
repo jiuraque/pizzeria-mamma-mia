d47475a221990dbe7be3e1a0206e9203d17044e1
jiuraque <jiulianna.raquel@gmail.com>
2025-04-20 23:25:38 -0400

    se modifica funcionabilidad de botones de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import Home from "./components/home/home";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 import Registro from "./components/Registro/Registro";
 import Login from "./components/Login/Login";
-
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Header /> */}
-     {/*  <Home /> */}
-     {/* <Registro /> */}
-     <Login />
+      <Header />
+      <Home />
+      <Routes>
+        <Route path="/Registro" element={<Registro />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
       <Footer />
     </>
   );
-};
+}
 
 export default App;
