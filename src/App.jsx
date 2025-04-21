706a899436d77ff0fe5514765d56d06779be6b5d
jiuraque <jiulianna.raquel@gmail.com>
2025-04-20 22:01:34 -0400

    Se comenta el registro y el login el main branch

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <Header /> */}
-     {/*  <Home /> */}
+      <Header />
+      <Home />
      {/* <Registro /> */}
-     <Login />
+    {/*  <Login /> */}
       <Footer />
     </>
   );
