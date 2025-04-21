2e7cfeba5a2ced6193aaf1dcc3d2c03d597d28ee
jiuraque <jiulianna.raquel@gmail.com>
2025-04-20 23:28:55 -0400

    updates

--- src/utils/TokenButton.jsx
+++ src/utils/TokenButton.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {useNavigate} from "react";
+import { useNavigate } from 'react-router-dom';
 import Button from "react-bootstrap/Button";
 
 function Token() {
