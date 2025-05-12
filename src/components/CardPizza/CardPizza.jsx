4343c9d71aab9e98b9d47049a6d8cb694103b98d
jiuraque <jiulianna.raquel@gmail.com>
2025-05-12 00:20:56 -0400

    se arreglan las tarjetas con grid

--- src/components/CardPizza/CardPizza.jsx
+++ src/components/CardPizza/CardPizza.jsx
@@ -1,45 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatPrice } from "../../utils/FormatPrice";
 import "./CardPizza.css";
 
 
 const CardPizza = ({ name, price, ingredients, img, desc }) => {
+  const [expandir, setExpandir] = useState(false);
+
   return (
-    <div className="card card-pizza shadow-sm d-flex">
-      <div className="card-body">
-        <div className="pizza-info text-center">
-          <h5 className="card-title">{name}</h5>
-
-          <hr className="divider" />
-
-          <p className="card-price">
-            <strong>Precio:</strong> ${formatPrice(price)}
-          </p>
-       
-
+    <div className="card card-pizza shadow-sm d-flex flex-column">
+    <img src={img} className="card-img-top pizza-img" alt={name} />
+    
+    <div className="card-body d-flex flex-column justify-content-between">
+      <div className="pizza-info text-center">
+        <h5 className="card-title">{name}</h5>
+  
         <hr className="divider" />
-
-          <ul className="card-ingredients">
-          🍕{ingredients.join(', ')}
-          </ul>
-
-          <hr className="divider" />
-
-          <img src={img} className="card-img-top pizza-img" alt={name} />
-
-          <hr className="divider" />
-
-          <p className="card-desc">🍕{desc}</p>
-
-        <div className="d-flex justify-content-between">
-          <button className="btn btn-outline-dark btn-sm fixed-btn">
-            Ver más 👀
+  
+        <p className="card-price">
+          <strong>Precio:</strong> ${formatPrice(price)}
+        </p>
+  
+        <hr className="divider" />
+  
+        <p className="card-ingredients">🍕 {ingredients.join(", ")}</p>
+  
+        <hr className="divider" />
+  
+        <p className={`card-desc ${expandir ? "expandida" : "reducida"}`}>🍕 {desc}</p>
+        <button
+            className="btn btn-sm text-decoration-none"
+            onClick={() => setExpandir(!expandir)}
+          >
+            {expandir ? "▲" : "▼"}
           </button>
-          <button className="btn btn-dark btn-sm fixed-btn">Añadir 🛒</button>
-        </div>
+      </div>
+  
+      <div className="d-flex justify-content-between mt-3">
+        <button className="btn btn-outline-dark btn-sm fixed-btn">
+          Ver más 👀
+        </button>
+        <button className="btn btn-dark btn-sm fixed-btn">Añadir 🛒</button>
       </div>
     </div>
-    </div>
+  </div>  
   );
 };
 
