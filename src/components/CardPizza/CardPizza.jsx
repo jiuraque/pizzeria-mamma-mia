3b33a422fd0866451d5f288036aa507bb1439099
jiuraque <jiulianna.raquel@gmail.com>
2025-05-19 01:13:36 -0400

    se modifica la navbar y el cart para que se vea el precio de los productos

--- src/components/CardPizza/CardPizza.jsx
+++ src/components/CardPizza/CardPizza.jsx
@@ -1,49 +1,60 @@
 import React, { useState } from "react";
 import { formatPrice } from "../../utils/FormatPrice";
 import "./CardPizza.css";
+import { useCart } from "../../Context/CartContext"; 
 
-
-const CardPizza = ({ name, price, ingredients, img, desc }) => {
-  const [expandir, setExpandir] = useState(false);
+const CardPizza = ({ id, name, price, img, ingredients, desc }) => {
+  const { addToCart } = useCart();
+  const [expandir, setExpandir] = useState(false); 
 
   return (
     <div className="card card-pizza shadow-sm d-flex flex-column">
-    <img src={img} className="card-img-top pizza-img" alt={name} />
-    
-    <div className="card-body d-flex flex-column justify-content-between">
-      <div className="pizza-info text-center">
-        <h5 className="card-title">{name}</h5>
-  
-        <hr className="divider" />
-  
-        <p className="card-price">
-          <strong>Precio:</strong> ${formatPrice(price)}
-        </p>
-  
-        <hr className="divider" />
-  
-        <p className="card-ingredients">🍕 {ingredients.join(", ")}</p>
-  
-        <hr className="divider" />
-  
-        <p className={`card-desc ${expandir ? "expandida" : "reducida"}`}>🍕 {desc}</p>
-        <button
+      <img src={img} className="card-img-top pizza-img" alt={name} />
+
+      <div className="card-body d-flex flex-column justify-content-between">
+        <div className="pizza-info text-center">
+          <h5 className="card-title">{name}</h5>
+
+          <hr className="divider" />
+
+          <p className="card-price">
+            <strong>Precio:</strong> ${formatPrice(price)}
+          </p>
+
+          <hr className="divider" />
+
+          <p className="card-ingredients">
+            {ingredients ? `🍕 ${ingredients.join(", ")}` : "Ingredientes no disponibles"}
+          </p>
+
+          <hr className="divider" />
+
+          <p className={`card-desc ${expandir ? "expandida" : "reducida"}`}>
+            {desc ? `🍕 ${desc}` : "Sin descripción"}
+          </p>
+          <button
             className="btn btn-sm text-decoration-none"
             onClick={() => setExpandir(!expandir)}
           >
             {expandir ? "▲" : "▼"}
           </button>
-      </div>
-  
-      <div className="d-flex justify-content-between mt-3">
-        <button className="btn btn-outline-dark btn-sm fixed-btn">
-          Ver más 👀
-        </button>
-        <button className="btn btn-dark btn-sm fixed-btn">Añadir 🛒</button>
+        </div>
+
+        <div className="d-flex justify-content-between mt-3">
+          <button className="btn btn-outline-dark btn-sm fixed-btn">
+            Ver más 👀
+          </button>
+          <button
+            className="btn btn-dark btn-sm fixed-btn"
+            onClick={() => addToCart({ id, name, price, img, ingredients, desc })}
+          >
+            Añadir 🛒
+          </button>
+        </div>
       </div>
     </div>
-  </div>  
   );
 };
 
 export default CardPizza;
+
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,43 +1,39 @@
+import { useCart } from "../../Context/CartContext";
 import "./Navbar.css";
 import Token from "../../utils/TokenButton";
 import Button from "react-bootstrap/Button";
 import { formatPrice } from "../../utils/FormatPrice";
 import { Navbar as BsNavbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useCart } from "../../Context/CartContext"; 
 
 function Navbar() {
-  const { total } = useCart(); 
+  const { total } = useCart();
 
   return (
-    <>
-      <BsNavbar expand="md" bg="dark" variant="dark" className="px-4 py-3">
-        <Container fluid>
-          <BsNavbar.Brand href="#">Pizzería Mamma Mía!</BsNavbar.Brand>
+    <BsNavbar expand="md" bg="dark" variant="dark" className="px-4 py-3">
+      <Container fluid>
+        <BsNavbar.Brand href="#">Pizzería Mamma Mía!</BsNavbar.Brand>
 
-          <BsNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <BsNavbar.Toggle aria-controls="basic-navbar-nav" />
 
-          <BsNavbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto gap-2">
-              <Link to="/">
-                <Button variant="outline-light">🍕 Home</Button>
-              </Link>
-              <Token />
-            </Nav>
-            <div className="d-flex">
-              <Link to="/cart">
-                <Button variant="outline-primary">
-                  🛒 Total: ${formatPrice(total)}
-                </Button>
-              </Link>
-            </div>
-          </BsNavbar.Collapse>
-        </Container>
-      </BsNavbar>
-    </>
+        <BsNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto gap-2">
+            <Link to="/">
+              <Button variant="outline-light">🍕 Home</Button>
+            </Link>
+            <Token />
+          </Nav>
+          <div className="d-flex">
+            <Link to="/cart">
+              <Button variant="outline-primary">
+                🛒 Total: ${formatPrice(total)}
+              </Button>
+            </Link>
+          </div>
+        </BsNavbar.Collapse>
+      </Container>
+    </BsNavbar>
   );
 }
 
 export default Navbar;
-
-
