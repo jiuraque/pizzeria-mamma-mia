182e5b98bd6ab3e68df8a6191e2e3aaba8145ada
jiuraque <jiulianna.raquel@gmail.com>
2025-05-18 23:10:45 -0400

    contenido del CartContext

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,8 +1,42 @@
-import { createContext, useContext, useState} from 'react'
+import { createContext, useContext, useState } from "react";
 
+const CartContext = createContext();
+export const useCart = () => useContext(CartContext);
 
-export default function CartContext() {
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]); //
+
+  const addToCart = (pizza) => {
+    const exists = cart.find((p) => p.id === pizza.id);
+
+    if (exists) {
+      setCart(
+        cart.map((p) => (p.id === pizza.id ? { ...p, count: p.count + 1 } : p))
+      );
+    } else {
+      setCart([...cart, { ...pizza, count: 1 }]);
+    }
+  };
+
+  const increase = (id) => {
+    setCart(cart.map((p) => (p.id === id ? { ...p, count: p.count + 1 } : p)));
+  };
+
+  const decrease = (id) => {
+    setCart(
+      cart
+        .map((p) => (p.id === id ? { ...p, count: p.count - 1 } : p))
+        .filter((p) => p.count > 0)
+    );
+  };
+
+  const total = cart.reduce((sum, p) => sum + p.price * p.count, 0);
+
   return (
-    <div>CartContext</div>
-  )
-}
+    <CartContext.Provider
+      value={{ cart, addToCart, increase, decrease, total }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
